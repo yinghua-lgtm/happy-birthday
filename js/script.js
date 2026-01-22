e98260e92209f8b18a2ccf94239c99b592dfebbb
michelleangelica1224-lgtm <michelleangelica1224@gmail.com>
2026-01-22 14:19:36 +0700

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-const cake = document.getElementById("cake");
+const home = document.getElementById("home");
+
+document.addEventListener("click", () => {
+  var duration = 20 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    });
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    });
+  }, 250);
+});
+
+const bgm = new Audio("assets/audio.mp3");
+const cta = document.getElementById("cta");
+
+cta.addEventListener("click", () => {
+  if (bgm.paused) {
+    bgm.play();
+    bgm.loop = true; // Makes it repeat
+    bgm.volume = 1; // Set volume
+    cta.textContent = "出發➶";
+  }
+});
